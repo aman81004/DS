fcc79fff5f38e94312cd495477aa63adb30b0778
aman81004 <shaikhmohammedfazlurrahman@gmail.com>
2025-05-12 17:48:41 +0530

    DS

---
+++ 2D Array/WavePrint.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter no of rows of Matrix:";
+    cin>>m;
+    int n;
+    cout<<"Enter no of coloumns of Matrix:";
+    cin>>n;
+    int arr[m][n];
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[m][n];
+        }
+    }
+    cout<<"Array is:";
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+    }
+    cout<<endl;
+    for(int i=0;i<m;i++){
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int j=n-1;j>=0;j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+}
